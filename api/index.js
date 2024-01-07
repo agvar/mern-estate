821e0073e3993fab36e95a4bec00de064cd1f148
agvar <ajeebgeorge@gmail.com>
2024-01-06 23:28:03 -0500

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import UserRouter from './routes/user.route.js';
+
 dotenv.config();
 mongoose.connect(process.env.MONGODB_CONNECTION).then(()=>{
 console.log("connected to Mongodb database");
@@ -13,4 +15,6 @@ const app=express();
 
 app.listen(3000,()=>{
     console.log("Server is running ");
-})
+});
+
+app.use('/api/user',UserRouter);
