e4ad6f5a194989718ee312212c9f817c6db0e89b
agvar <ajeebgeorge@gmail.com>
2024-01-14 17:51:18 -0500

    complete sign-in page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,14 +19,16 @@ try {
 export const signin = async(req,res,next) => {
     const {email,password} =req.body;
        try{
-        console.log(req.body);
+        console.log("request",req.body);
         const validUser = await User.findOne({email});     
-        if (!validUser) return errorHandler(404,'User Name not Found!');
-        const validPassword = bcryptjs.compareSync(password,validUser.password);
-        if (!validPassword) return errorHandler(401,'Wrong Credentials');
 
+        if (!validUser) {return next(errorHandler(404,'User Name not Found!'));}
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+
+        if (!validPassword) {return next(errorHandler(401,'Wrong Credentials'));}
+        
         const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
-        const {password:pass,...rest} = validUser;
+        const {password:pass,...rest} = validUser._doc;
         res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
         console.log(rest);
     }
